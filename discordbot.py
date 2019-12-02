c4526c0fb78d539958379bf5d70a26f0d7420c65
E0deavor-se <58418565+E0deavor-se@users.noreply.github.com>
2019-12-02 20:34:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,14 +6,14 @@ import traceback
 client = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
+@client.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
 
-@bot.command()
+@client.command()
 async def ping(ctx):
     await ctx.send('pong')
 
